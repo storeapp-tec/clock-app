cb76d0a9b24e084525409b0fc33f7ec1f7b1ded5
storeapp <latynx@gmail.com>
2026-07-22 16:42:10 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,22 +2,29 @@ const CACHE_NAME = 'studio-clock-v1';
 const assets = [
   './',
   './index.html',
-  './manifest.json',
-  // Añade aquí tus iconos si quieres que funcionen offline
+  './manifest.json'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+// Instalar y cachear archivos
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
+// Activar y limpiar caches antiguos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Responder peticiones incluso sin internet
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
